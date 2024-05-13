c988c93ce8fe423f0911257f578c56903040a209
pinksealteam <153603468+pinksealteam@users.noreply.github.com>
2024-05-13 14:35:27 +0200

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -25,7 +25,7 @@ console.log('\x1b[32m%s\x1b[0m', `|    🌼 Logged in as ${client.user.username}
 
 const serverCount = client.guilds.cache.size;
 setInterval(() => client.user.setActivity({ 
-  name:`lavaLink GlaceYT`, 
+  name:`pedodobro333`, 
   type: ActivityType.Listening }), 10000);
 client.errorLog = config.errorLog
   
