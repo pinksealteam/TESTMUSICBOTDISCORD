20dcb1a0a6f41b48b00abbddf029fc54ade9bdfc
pinksealteam <153603468+pinksealteam@users.noreply.github.com>
2024-05-13 14:26:26 +0200

    Update play.js

--- commands/play.js
+++ commands/play.js
@@ -82,7 +82,7 @@ const embeds = [
         .setAuthor({
             name: 'Request Update!',
             iconURL: 'https://cdn.discordapp.com/attachments/1230824451990622299/1236794583732457473/7828-verify-ak.gif?ex=66394e37&is=6637fcb7&hm=923d3f3b300606a2ae4ceb7bae980fd533a4c5ee2cf73111569a892a595f1f69&', 
-            url: 'https://discord.gg/xQF9f9yUEM'
+            url: 'https://www.youtube.com/channel/UCPOwL09DQXKTVgpMTxQkQZA'
         })
         .setDescription('➡️ **Your request has been successfully processed.**\n➡️** Please use the buttons to control the queue**'),
 
@@ -92,7 +92,7 @@ const embeds = [
     .setAuthor({
         name: 'Request Update!',
         iconURL: 'https://cdn.discordapp.com/attachments/1230824451990622299/1236802032938127470/4104-verify-yellow.gif?ex=66395527&is=663803a7&hm=71a7fba7f91897e52d9645b45d85d3da0ff97af2b63d10960004e68ff40d9c3b&', 
-        url: 'https://discord.gg/xQF9f9yUEM'
+        url: 'https://www.youtube.com/channel/UCPOwL09DQXKTVgpMTxQkQZA'
     })
     .setDescription('➡️ **Your request has been successfully processed.**\n➡️** Please use the buttons to control the queue**'),
 
@@ -102,7 +102,7 @@ const embeds = [
     .setAuthor({
         name: 'Request Update!',
         iconURL: 'https://cdn.discordapp.com/attachments/1230824451990622299/1236802049190920202/4104-verify-red.gif?ex=6639552b&is=663803ab&hm=8dbc851fe56441b916a0044152dd517ed26434a11ee506518382f380c527c3bd&', 
-        url: 'https://discord.gg/xQF9f9yUEM'
+        url: 'https://www.youtube.com/channel/UCPOwL09DQXKTVgpMTxQkQZA'
     })
     .setDescription('➡️ **Your request has been successfully processed.**\n➡️** Please use the buttons to control the queue**')
 ];
@@ -139,23 +139,3 @@ module.exports = {
     queueNames: queueNames
 };
 
-/*
-
-  ________.__                        _____.___.___________
- /  _____/|  | _____    ____  ____   \__  |   |\__    ___/
-/   \  ___|  | \__  \ _/ ___\/ __ \   /   |   |  |    |   
-\    \_\  \  |__/ __ \\  \__\  ___/   \____   |  |    |   
- \______  /____(____  /\___  >___  >  / ______|  |____|   
-        \/          \/     \/    \/   \/                  
-
-╔════════════════════════════════════════════════════════════════════════╗
-║                                                                        ║
-║  ## Created by GlaceYT!                                                ║
-║  ## Feel free to utilize any portion of the code                       ║
-║  ## DISCORD :  https://discord.com/invite/xQF9f9yUEM                   ║
-║  ## YouTube : https://www.youtube.com/@GlaceYt                         ║
-║                                                                        ║
-╚════════════════════════════════════════════════════════════════════════╝
-
-
-*/
